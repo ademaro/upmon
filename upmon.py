54c9d1d3f1462c38c4b58e88d4e8712ce3bee3a9
koc <@komp.(none)>
2011-03-23 16:02:22 +0800

    def ins

--- upmon.py
+++ upmon.py
@@ -1,15 +1,21 @@
 import os, sys
 
-if (sys.platform == "win32"):
-    # win
-    pkey = 'n'
-else:
-    # posix
-    pkey = 'c'
+def ping_host(srv_hostname):
+    if (sys.platform == "win32"):
+        # win
+        pkey = 'n'
+    else:
+        # posix
+        pkey = 'c'
+    f = os.popen('ping -' + pkey + ' 1 ' + str(srv_hostname))
+    if f.read().lower().count('ttl'):
+        return True
 
 for hostname in open('servers.list'):
-    f = os.popen('ping -' + pkey + ' 1' + str(hostname))
-    if f.read().lower().count('ttl'):
-        print(("{0} OK").format(hostname))
+    if ping_host(hostname) is True:
+        print(hostname[:(len(hostname)-1)] + ' is OK')
     else:
-        print(("{0} NOK").format(hostname))
+        print(hostname[:(len(hostname)-1)] + ' is DOWN')
+
+
+
