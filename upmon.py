8478679133be8d958b60e2a72bcfdfbed334a63e
Anton <ademaro@ya.ru>
2011-03-23 08:46:30 +0800

    some clean code

--- upmon.py
+++ upmon.py
@@ -1,38 +1,15 @@
 import os, sys
 
 if (sys.platform == "win32"):
-        # win
-    ping_arg = '-n 1'
+    # win
+    pkey = 'n'
 else:
-        # posix
-    ping_arg = '-c 1'
-#else:
-#    print ('platform ' + sys.platform + ' is not supported')
+    # posix
+    pkey = 'c'
 
-for c in open('servers.list'):
-        f = os.popen("ping "+ ping_arg+ ' ' + str(c))
-        if f.read().lower().count('ttl'):
-            print(("{0} DONE").format(c))
-        else:
-            print(("{0} is down").format(c))
-#    else:
-#        # POSIX
-#        f = os.popen("ping -c 1 " + str(c))
-#        if f.read().count('ttl'):
-#            print(("{0} DONE").format(c))
-#        else:
-#            print(("{0} is down").format(c))
-
-# cmd1 = 'ping -n 1 ' + str(c)
-# f = subprocess.getstatusoutput(cmd1)
-# print(str(f).encode())
-# f = str(f)
-# print(f)
-# if f.count('TTL=62') > 0:
-# print('DONE '*5)
-# else:
-# print('FALSE '*5)
-# for ttl in f[0:]:
-# print (ttl)
-# if 'TTL' in ttl:
-# print('done')
+for hostname in open('servers.list'):
+    f = os.popen('ping -' + pkey + ' 1' + str(hostname))
+    if f.read().lower().count('ttl'):
+        print(("{0} OK").format(hostname))
+    else:
+        print(("{0} NOK").format(hostname))
